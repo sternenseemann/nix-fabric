4578575f7620eb930cb3e784b54f1ce39083a90f
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-19 16:40:31 +0100

    refactor(treewide): move nix expression into subdirs

--- default.nix
+++ default.nix
@@ -4,8 +4,8 @@ let
   inherit (pkgs.lib) fix;
 
   common = self: {
-    buildMinecraftJar = self.callPackage ./build-minecraft-jar.nix { };
-  } // import ./jars-common.nix self;
+    buildMinecraftJar = self.callPackage ./build-support/build-minecraft-jar.nix { };
+  } // import ./pkgs/jars-common.nix self;
 
   minecraftVersionSet = minecraftVersion: jars:
     fix (self: common self // {
@@ -17,16 +17,16 @@ let
         inherit (pkgs) stdenv fetchurl runCommandLocal;
       } // self);
 
-      buildFabricModsDir = self.callPackage ./build-fabric-mods-dir.nix {
+      buildFabricModsDir = self.callPackage ./build-support/build-fabric-mods-dir.nix {
         fabricPackages = self;
       };
 
       buildMasaMod =
-        self.callPackage ./build-masa-mod.nix { } self.minecraftVersion;
+        self.callPackage ./build-support/build-masa-mod.nix { } self.minecraftVersion;
     } // import jars self);
 
 in rec {
   fabricPackages = fabricPackages_1_16_4;
-  fabricPackages_1_16_3 = minecraftVersionSet "1.16.3" ./jars-1.16.3.nix;
-  fabricPackages_1_16_4 = minecraftVersionSet "1.16.4" ./jars-1.16.4.nix;
+  fabricPackages_1_16_3 = minecraftVersionSet "1.16.3" ./pkgs/jars-1.16.3.nix;
+  fabricPackages_1_16_4 = minecraftVersionSet "1.16.4" ./pkgs/jars-1.16.4.nix;
 }
