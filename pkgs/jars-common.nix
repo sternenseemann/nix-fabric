a9d83dee0e5f88fc7fa0be49c27392e8e365da4c
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-19 17:17:16 +0100

    refactor(common): use version for url

--- pkgs/jars-common.nix
+++ pkgs/jars-common.nix
@@ -3,10 +3,10 @@ self:
 with self;
 
 {
-  fabric-installer = buildMinecraftJar {
+  fabric-installer = buildMinecraftJar rec {
     pname = "fabric-installer";
     version = "0.6.1.51";
-    url = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.6.1.51/fabric-installer-0.6.1.51.jar";
+    url = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/${version}/fabric-installer-${version}.jar";
     hash = "sha256:0cima0n3b37qha9a16kcvjnx9mg231v5wdg1063gxnq3vrxlcw23";
     meta = {
       homepage = "https://github.com/FabricMC/fabric-installer";
