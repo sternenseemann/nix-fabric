1deddd6566715a4c50b9f36a364efb617796140e
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-01-20 20:25:34 +0100

    refactor(treewide): hash → sha256

--- pkgs/jars-1.16.3.nix
+++ pkgs/jars-1.16.3.nix
@@ -7,7 +7,7 @@ with self;
     pname = "server";
     version = minecraftVersion;
     url = "https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar";
-    hash = "sha256:0nxdyw23037cr9cfcsfq1cvpy75am5dzmbgvvh3fq6h89kkm1r1j";
+    sha256 = "0nxdyw23037cr9cfcsfq1cvpy75am5dzmbgvvh3fq6h89kkm1r1j";
     meta = {
       description = "Minecraft multiplayer server";
       license = lib.licenses.unfree;
@@ -24,7 +24,7 @@ with self;
       pname = "carpet";
       version = "${version}-${date}";
       url = "https://github.com/gnembon/fabric-carpet/releases/download/v1.4-homebound/fabric-carpet-${minecraftVersion}-${version}+v${urldate}.jar";
-      hash = "sha256:1cvb753b1y59w3d8h2mkp4254w0ckrmwfa42znh0f6qz6jpd14kq";
+      sha256 = "1cvb753b1y59w3d8h2mkp4254w0ckrmwfa42znh0f6qz6jpd14kq";
       meta = {
         description = "Carpet Mod is a mod for vanilla Minecraft that allows you to take full control of what matters from a technical perspective of the game";
         license = lib.licenses.mit;
@@ -39,7 +39,7 @@ with self;
     version = "0.15.0-dev";
     date = "2020-09-12";
     time = "22:18:05";
-    hash = "sha256:0zgj9krxs5dxdb5w8jv7ryjld06z5hb81rpaszlnh85l021n52i7";
+    sha256 = "0zgj9krxs5dxdb5w8jv7ryjld06z5hb81rpaszlnh85l021n52i7";
     dependencies = [ malilib ];
     meta = {
       description = "Tiny Minecraft mod that allows moving items by scrolling over the inventory slots";
@@ -52,7 +52,7 @@ with self;
     version = "0.0.0-dev";
     date = "2020-09-20";
     time = "16:16:40";
-    hash = "sha256:02d8bm2c6f0rwagxlvgcc1k109mw0a5yrala4hw54qx7j7h7as12";
+    sha256 = "02d8bm2c6f0rwagxlvgcc1k109mw0a5yrala4hw54qx7j7h7as12";
     dependencies = [ malilib ];
     meta = {
       description = "Litematica is a client-side schematic mod for Minecraft";
@@ -62,7 +62,7 @@ with self;
   malilib = fetchMasaMod {
     pname = "malilib";
     version = "0.10.0-dev.21+arne.1";
-    hash = "sha256:080jszf61ac07yxcrxgzix4cy1mrvqahbcipny534p13cpz0qrbc";
+    sha256 = "080jszf61ac07yxcrxgzix4cy1mrvqahbcipny534p13cpz0qrbc";
     meta = {
       description = "malilib is a library mod used by masa's LiteLoader mods";
       license = lib.licenses.gpl3Only;
@@ -74,7 +74,7 @@ with self;
     version = "0.19.0-dev";
     date = "2020-10-27";
     time = "14:54:41";
-    hash = "sha256:0krmvrb82kgj5vq09dxcrv0jw5xza92pk0xb54j6i8dj7bzmddix";
+    sha256 = "0krmvrb82kgj5vq09dxcrv0jw5xza92pk0xb54j6i8dj7bzmddix";
     dependencies = [ malilib ];
     meta = {
       description = "MiniHUD is a client-side information and overlay rendering mod for Minecraft";
@@ -86,7 +86,7 @@ with self;
     version = "0.10.0-dev";
     date = "2020-10-04";
     time = "19:18:11";
-    hash = "sha256:18b8qjza10ylsig6ca1kwn4n98dwlvb5ns2rgar859acvnk75h51";
+    sha256 = "18b8qjza10ylsig6ca1kwn4n98dwlvb5ns2rgar859acvnk75h51";
     dependencies = [ malilib ];
     meta = {
       description = "Tweakeroo adds a selection of miscellaneous, configurable, client-side tweaks to the game";
--- pkgs/jars-1.16.4.nix
+++ pkgs/jars-1.16.4.nix
@@ -7,7 +7,7 @@ with self;
     pname = "server";
     version = minecraftVersion;
     url = "https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar";
-    hash = "sha256:01i5nd03sbnffbyni1fa6hsg5yll2h19vfrpcydlivx10gck0ka4";
+    sha256 = "01i5nd03sbnffbyni1fa6hsg5yll2h19vfrpcydlivx10gck0ka4";
     meta = {
       description = "Minecraft multiplayer server";
       license = lib.licenses.unfree;
@@ -24,7 +24,7 @@ with self;
       pname = "carpet";
       version = "${version}-${date}";
       url = "https://github.com/gnembon/fabric-carpet/releases/download/v1.4-homebound/fabric-carpet-${minecraftVersion}-${version}+v${urldate}.jar";
-      hash = "sha256:1vzcwh4n6gpdb76z8w4yrkddwvz72fjvbxkf3cbncvqvmv7dvpzp";
+      sha256 = "1vzcwh4n6gpdb76z8w4yrkddwvz72fjvbxkf3cbncvqvmv7dvpzp";
       meta = {
         description = "Carpet Mod is a mod for vanilla Minecraft that allows you to take full control of what matters from a technical perspective of the game";
         license = lib.licenses.mit;
@@ -39,7 +39,7 @@ with self;
     version = "0.15.0-dev";
     date = "2020-11-03";
     time = "18:39:33";
-    hash = "sha256:0g2jxg8rnns1x94j46vh2xj6mmz054gms709fqkkhvd6pdb0rnim";
+    sha256 = "0g2jxg8rnns1x94j46vh2xj6mmz054gms709fqkkhvd6pdb0rnim";
     dependencies = [ malilib ];
     meta = {
       description = "Tiny Minecraft mod that allows moving items by scrolling over the inventory slots";
@@ -52,7 +52,7 @@ with self;
     version = "0.0.0-dev";
     date = "2020-11-03";
     time = "18:41:01";
-    hash = "sha256:1v2w587py2f7pyh0cy84mv3akipmqx2dn29540cfprzhfdn60x4a";
+    sha256 = "1v2w587py2f7pyh0cy84mv3akipmqx2dn29540cfprzhfdn60x4a";
     dependencies = [ malilib ];
     meta = {
       description = "Litematica is a client-side schematic mod for Minecraft";
@@ -62,7 +62,7 @@ with self;
   malilib = fetchMasaMod {
     pname = "malilib";
     version = "0.10.0-dev.21+arne.2";
-    hash = "sha256:1h629bmp4dr9f0vcvlxdlji3cjrp06qdasy5mgwdvqg33gazg3qz";
+    sha256 = "1h629bmp4dr9f0vcvlxdlji3cjrp06qdasy5mgwdvqg33gazg3qz";
     meta = {
       description = "malilib is a library mod used by masa's LiteLoader mods";
       license = lib.licenses.gpl3Only;
@@ -74,7 +74,7 @@ with self;
     version = "0.19.0-dev";
     date = "2020-11-03";
     time = "18:40:29";
-    hash = "sha256:1dngxwjvqag7kf14vqj2rjdwvqkdkkw649k5zrgvq6clrbwn1jnr";
+    sha256 = "1dngxwjvqag7kf14vqj2rjdwvqkdkkw649k5zrgvq6clrbwn1jnr";
     dependencies = [ malilib ];
     meta = {
       description = "MiniHUD is a client-side information and overlay rendering mod for Minecraft";
@@ -86,7 +86,7 @@ with self;
     version = "0.10.0-dev";
     date = "2020-11-22";
     time = "17:48:05";
-    hash = "sha256:1y7f8apis7kd05c12wfx5c0bxj4q8nnc80ml0rpjhl7n3ydikxml";
+    sha256 = "1y7f8apis7kd05c12wfx5c0bxj4q8nnc80ml0rpjhl7n3ydikxml";
     dependencies = [ malilib ];
     meta = {
       description = "Tweakeroo adds a selection of miscellaneous, configurable, client-side tweaks to the game";
